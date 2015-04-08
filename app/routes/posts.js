2901460a3ba9fd8aacfe0b128ef31a16f921213d
Joe Heth <joeheth@gmail.com>
2015-04-07 23:22:44 -0400

    Adds new-post component using ember-idx-modal addon.

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('post');
+  },
+  actions: {
+    savePost: function(data) {
+      var post = this.store.createRecord('post', data);
+      post.save();
+
+      // var posts = this.controllerFor('posts');
+      // posts.get('model').pushObject(post);
+    }
   }
 });
