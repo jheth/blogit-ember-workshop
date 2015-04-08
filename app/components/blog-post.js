c6d031467c4607eebf61165bff7b72018cc461a1
Joe Heth <joeheth@gmail.com>
2015-04-07 22:54:50 -0400

    Implement blog-post edit/save actions.

--- app/components/blog-post.js
+++ app/components/blog-post.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isEditing: false,
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    save: function() {
+      this.set('isEditing', false);
+      this.get('post').save();
+    }
+  }
 });
